d9f84167a571eb2a73b08eb239590eecfbb85092
yougan14 <myouganmurugan@gmail.com>
2021-12-10 10:36:59 +0530

    Add users create action

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-
+before_save { self.email = email.downcase }
     validates :username, presence: true, 
                         uniqueness: { case_sensitive: false }, 
                         length: { minimum: 3, maximum: 25 }
@@ -8,4 +8,5 @@ class User < ApplicationRecord
                         uniqueness: { case_sensitive: false }, 
                         length: { maximum: 105 },
                         format: { with: VALID_EMAIL_REGEX }
-  end
+  has_secure_password
+                    end
