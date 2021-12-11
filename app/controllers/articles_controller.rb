3d67af338cb6507e9da96db76a75bde23062dc0e
yougan14 <myouganmurugan@gmail.com>
2021-12-11 17:11:13 +0530

    first commit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,68 +1,45 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: %i[ :show, :edit, :update, :destroy ]
-
-  # GET /articles or /articles.json
-  def index
-   @articles = Article.all
-  end
-
-  # GET /articles/1 or /articles/1.json
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
   def show
-    @article = Article.find(params[:id])
   end
 
-  # GET /articles/new
+  def index
+    @articles = Article.all
+    #@articles = Article.paginate(page: params[:page], per_page: 5)
+  end
+
   def new
     @article = Article.new
   end
-
-  # GET /articles/1/edit
   def edit
-    
-    @article = Article.find(params[:id])
   end
-
-  # POST /articles or /articles.json
   def create
-    @article = Article.new(params.require(:article).permit(:title,:description))
-    @article.user = User.first 
+    @article = Article.new(article_params)
+    @article.user = User.first
     if @article.save
-      flash[:notice] = "article was created successfully...."
-    redirect_to @article  
+      flash[:notice] = "Article was created successfully."
+      redirect_to @article
     else
-    render 'new'
+      render 'new'
     end
   end
-
-  # PATCH/PUT /articles/1 or /articles/1.json
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title,:description))
-    flash[:notice] = "Article was updated successfully."
-    redirect_to @article
+    if @article.update(article_params)
+      flash[:notice] = "Article was updated successfully."
+      redirect_to @article
     else
       render 'edit'
     end
   end
-
-
-def destroy
-  @article = Article.find(params[:id])
-  @article.destroy
-  redirect_to articles_path
-end
-
-
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_article
-      @article = Article.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def article_params
-      params.require(:article).permit(:title, :description)
-    end
-
+  def destroy
+    @article.destroy
+    redirect_to articles_path
   end
+  private
+  def set_article
+    @article = Article.find(params[:id])
+  end
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,42 @@
 class UsersController < ApplicationController
-    def new
-        @user = User.new
+  def show
+    @user = User.find(params[:id])
+    @articles = @user
+    
+  end
+
+  def index
+   
+    @users = User.paginate(page: params[:page], per_page: 5)
+  end
+
+  def new
+    @user = User.new
+  end
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "Your account information was successfully updated"
+      redirect_to @user
+    else
+      render 'edit'
     end
-    def create
-@user = User.new(user_params)
-if @user.save
-    flash[:notice]= "welcome to articles #{@user.username}, you have have successfully signed up"
-    redirect_to articles_path
-else
-    render 'new'
-end
+  end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome to the Articles #{@user.username}, you have successfully signed up"
+      redirect_to articles_path
+    else
+      render 'new'
     end
-    private
-    def user_params
-        params.require(:user).permit(:username, :email, :password)
-    end
+  end
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+  
 end
